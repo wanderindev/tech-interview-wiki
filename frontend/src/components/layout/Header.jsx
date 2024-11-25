8c032c57973610c01c3a834f4c37d7fb699d8159
Javier Feliu <javier@monetizemore.com>
2024-11-25 12:12:48 -0500

    Center logo within header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -3,23 +3,13 @@ import {Link} from 'react-router-dom';
 export default function Header() {
   return (
     <header className="sticky top-0 bg-white shadow-[0px_7px_12px_-10px_rgba(0,0,0,0.2)] h-[100px]">
-      <div className="mx-auto max-w-[1140px] h-full px-4">
-        <div className="flex items-center justify-between h-full">
-          <Link
-            to="/"
-            className="font-['M_PLUS_Code_Latin'] text-black text-[20px] font-normal"
-          >
-            {'{ tech-interview.wiki }'}
-          </Link>
-
-          <div className="flex-1 mx-8">
-            {/* Filter component will go here */}
-          </div>
-
-          <div className="w-[300px]">
-            {/* Search component will go here */}
-          </div>
-        </div>
+      <div className="mx-auto max-w-[1140px] h-full px-4 flex items-center justify-center">
+        <Link
+          to="/"
+          className="font-['M_PLUS_Code_Latin'] text-black text-[20px] font-normal whitespace-nowrap"
+        >
+          {'{ tech-interview.wiki }'}
+        </Link>
       </div>
     </header>
   );
